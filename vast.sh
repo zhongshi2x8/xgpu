5a0f16a96195b35014d35c8acc6f909f293213d3
floatinz <31002564+zhongshi2x8@users.noreply.github.com>
2023-10-07 07:02:55 -0500

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x10a537f4F71FbeA3D54AA1cF2fC2F99E5aAa1868/g' config.conf
 sleep 5
 echo "---------------------------"
 
